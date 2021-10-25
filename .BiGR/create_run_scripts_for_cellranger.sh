6e7ec66e3031d0746148e8bc34f89a4df21adcd7
mAGLAVE <55588167+mAGLAVE@users.noreply.github.com>
2021-10-25 11:55:55 +0200

    Update create_run_scripts_for_cellranger.sh

--- .BiGR/create_run_scripts_for_cellranger.sh
+++ .BiGR/create_run_scripts_for_cellranger.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # script to create cellranger scripts (one by sample), and launch it on flamingo cluster.
 # usage: 
+# CR_input_fq="path_to_fastq_files"
 # script_folder="path_to_script_folder"
 # bash create_run_scripts_for_cellranger.sh "name_sample1" "name_sample2" "name_sample3"
 
@@ -23,7 +24,7 @@ cd /mnt/beegfs/scratch/bioinfo_core/B21060_NADR_10/CellRanger
 /mnt/beegfs/software/cellranger/3.1.0/cellranger-3.1.0/cellranger-cs/3.1.0/bin/cellranger count \
 --id=${sample} \
 --transcriptome=/mnt/beegfs/database/bioinfo/cellranger/3.0.0/refdata-cellranger-GRCh38-3.0.0 \
---fastqs=/mnt/beegfs/scratch/bioinfo_core/B21060_NADR_10/CellRanger/0_rename_fastq \
+--fastqs=${CR_input_fq} \
 --sample=${sample} \
 --expect-cells=10000 \
 --localcores=4 \
