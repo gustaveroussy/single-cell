f1df9a1ffc12ab7ac74e0ece711bbada02e656af
mAGLAVE <55588167+mAGLAVE@users.noreply.github.com>
2021-10-28 10:08:32 +0200

    Update create_run_scripts_for_cellranger.sh
    Add transcriptome choice for human or mouse

--- .BiGR/create_run_scripts_for_cellranger.sh
+++ .BiGR/create_run_scripts_for_cellranger.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # script to create cellranger scripts (one by sample), and launch it on flamingo cluster.
 # usage: 
-# CR_input_fq="path_to_fastq_files"
-# CR_output="path_to_output"
-# script_folder="path_to_script_folder"
+# export CR_input_fq="path_to_fastq_files"
+# export CR_output="path_to_output"
+# export script_folder="path_to_script_folder"
+# export transcriptome="/mnt/beegfs/database/bioinfo/cellranger/3.0.0/refdata-cellranger-mm10-3.0.0" #human by default
 # bash create_run_scripts_for_cellranger.sh "name_sample1" "name_sample2" "name_sample3"
 
 all_samples=${*}
+
+if [ -z "$transcriptome" ]; then transcriptome="/mnt/beegfs/database/bioinfo/cellranger/3.0.0/refdata-cellranger-GRCh38-3.0.0"; fi
 
 for sample in ${all_samples}
 do
@@ -25,7 +28,7 @@ cd ${CR_output}
 
 /mnt/beegfs/software/cellranger/3.1.0/cellranger-3.1.0/cellranger-cs/3.1.0/bin/cellranger count \
 --id=${sample} \
---transcriptome=/mnt/beegfs/database/bioinfo/cellranger/3.0.0/refdata-cellranger-GRCh38-3.0.0 \
+--transcriptome=${transcriptome} \
 --fastqs=${CR_input_fq} \
 --sample=${sample} \
 --expect-cells=10000 \
@@ -37,5 +40,4 @@ EOT
 sbatch ${script_folder}'/launcher_'${sample}'.sh'
 
 done
-
 
