17236d1a983ed3df1950bf5c4526ae791f8b24da
mAGLAVE <marine.aglave@gustaveroussy.fr>
2026-01-14 17:22:48 +0100

    add individual results example

--- examples/complete_individual_analysis_example_of_wiki/launcher.sh
+++ examples/complete_individual_analysis_example_of_wiki/launcher.sh
@@ -3,7 +3,7 @@
 ########################################################################
 ## Single-cell script to launch single-cell pipeline
 ##
-## using: sbatch /mnt/beegfs/userdata/m_aglave/pipeline/single-cell/examples/complete_individual_analysis_example_of_wiki/launcher.sh
+## using: sbatch /mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/examples/complete_individual_analysis_example_of_wiki/launcher.sh
 ########################################################################
 #SBATCH --job-name=pipeline_sc
 #SBATCH --nodes=1
@@ -11,13 +11,21 @@
 #SBATCH --mem=1G
 #SBATCH --partition=mediumq
 
-conda activate /mnt/beegfs/userdata/m_aglave/.environnement_conda/single_cell_user
-module load singularity
+source /mnt/beegfs02/software/recherche/miniconda/25.1.1/etc/profile.d/conda.sh
+conda activate /mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/envs/compiled_conda/snakemake
+module load singularity/3.10.5
 
+#print environment tools versions
+python --version
+snakemake --version
+singularity --version
 
-path_to_configfile="/mnt/beegfs/userdata/m_aglave/pipeline/single-cell/examples/complete_individual_analysis_example_of_wiki/Params.yaml"
-path_to_pipeline="/mnt/beegfs/pipelines/single-cell"
+#parameters
+path_to_configfile="/mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/examples/complete_individual_analysis_example_of_wiki/Params.yaml"
+path_to_pipeline="/mnt/beegfs02/pipelines/bigr_single-cell/2.0.0"
 
+#launch
 snakemake --profile ${path_to_pipeline}/profiles/slurm -s ${path_to_pipeline}/Snakefile --configfile ${path_to_configfile}
+
 
 conda deactivate
