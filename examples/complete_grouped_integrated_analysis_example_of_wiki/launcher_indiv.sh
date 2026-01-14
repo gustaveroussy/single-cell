cba1b15187aa530ae211465ecc252b94e648c773
mAGLAVE <marine.aglave@gustaveroussy.fr>
2026-01-14 18:37:10 +0100

    add grouped integrated script example

--- examples/complete_grouped_integrated_analysis_example_of_wiki/launcher_indiv.sh
+++ examples/complete_grouped_integrated_analysis_example_of_wiki/launcher_indiv.sh
@@ -3,7 +3,7 @@
 ########################################################################
 ## Single-cell RNA-seq script to launch single-cell RNA-seq pipeline
 ##
-## using: sbatch /mnt/beegfs/userdata/m_aglave/pipeline/single-cell/examples/complete_grouped_integrated_analysis_example_of_wiki/launcher_indiv.sh
+## using: sbatch /mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/examples/complete_grouped_integrated_analysis_example_of_wiki/launcher_indiv.sh
 ##
 ########################################################################
 
@@ -13,21 +13,22 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
-#SBATCH --partition=mediumq
+#SBATCH --partition=shortq
 
-source /mnt/beegfs/software/conda/etc/profile.d/conda.sh
-conda activate /mnt/beegfs/userdata/m_aglave/.environnement_conda/scRNAseq_10X_user
-module load singularity
+source /mnt/beegfs02/software/recherche/miniconda/25.1.1/etc/profile.d/conda.sh
+conda activate /mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/envs/compiled_conda/snakemake
+module load singularity/3.10.5
 
 python --version
 snakemake --version
 singularity --version
 
 #parameters
-path_to_configfile="/mnt/beegfs/userdata/m_aglave/pipeline/single-cell/examples/complete_grouped_integrated_analysis_example_of_wiki/Params_indiv.yaml"
-path_to_pipeline="/mnt/beegfs/pipelines/single-cell"
+path_to_configfile="/mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/examples/complete_grouped_integrated_analysis_example_of_wiki/Params_indiv.yaml"
+path_to_pipeline="/mnt/beegfs02/pipelines/bigr_single-cell/2.0.0"
 
 #launch
-snakemake --profile ${path_to_pipeline}/profiles/slurm -s ${path_to_pipeline}/Snakefile --configfile ${path_to_configfile}
+snakemake --profile ${path_to_pipeline}/profiles/slurm -s ${path_to_pipeline}/Snakefile --configfile ${path_to_configfile} --attempt 5
+ #--dry-run
 
 conda deactivate
--- examples/complete_grouped_integrated_analysis_example_of_wiki/launcher_int_harmony.sh
+++ examples/complete_grouped_integrated_analysis_example_of_wiki/launcher_int_harmony.sh
@@ -3,13 +3,13 @@
 ########################################################################
 ## Single-cell RNA-seq script to launch single-cell RNA-seq pipeline
 ##
-## using: sbatch /mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/examples/complete_grouped_integrated_analysis_example_of_wiki/launcher_grp.sh
+## using: sbatch /mnt/beegfs02/userdata/m_aglave/pipeline/bigr_single-cell/2.0.0/examples/complete_grouped_integrated_analysis_example_of_wiki_old/launcher_int_harmony.sh
 ##
 ########################################################################
 
 ## JOB PARAMETERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-#SBATCH --job-name=dev_grp
+#SBATCH --job-name=dev_harmony
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
@@ -24,10 +24,10 @@ snakemake --version
 singularity --version
 
 #parameters
-path_to_configfile="/mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/examples/complete_grouped_integrated_analysis_example_of_wiki/Params_grp.yaml"
+path_to_configfile="/mnt/beegfs02/pipelines/bigr_single-cell/2.0.0/examples/complete_grouped_integrated_analysis_example_of_wiki/Params_int_harmony.yaml"
 path_to_pipeline="/mnt/beegfs02/pipelines/bigr_single-cell/2.0.0"
 
 #launch
-snakemake --profile ${path_to_pipeline}/profiles/slurm -s ${path_to_pipeline}/Snakefile --configfile ${path_to_configfile} #--dry-run
+snakemake --profile ${path_to_pipeline}/profiles/slurm -s ${path_to_pipeline}/Snakefile --configfile ${path_to_configfile}
 
 conda deactivate
